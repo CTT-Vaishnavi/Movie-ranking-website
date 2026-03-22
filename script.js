e1df0afd8940b9753d8d3b434cdc02bb190efd0a
vaishnavi <shindevai2004@gmail.com>
2026-03-22 21:26:40 +0530

    git commit

--- script.js
+++ script.js
@@ -1,129 +1,217 @@
-// 🎬 MOVIE DATA
+// Fixed working script.js - No errors
+
 const movies = [
-  {title:"Spider Man",genre:"Action",year:2021,img:"https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", trailer: "https://youtu.be/5PSNL1qE6VY?si=4ei0DlRHI0TvUErc"},
-  {title:"Wonder Woman",genre:"Adventure",year:2020,img:"https://image.tmdb.org/t/p/w500/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg"},
-  {title:"Batman",genre:"Action",year:2022,img:"https://image.tmdb.org/t/p/w500/74xTEgt7R36Fpooo50r9T25onhq.jpg"},
-  {title:"Joker",genre:"Drama",year:2019,img:"https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg"},
-  {title:"Thor",genre:"Action",year:2022,img:"https://image.tmdb.org/t/p/w500/pIkRyD18kl4FhoCNQuWxWu5cBLM.jpg"},
-  {title:"Iron Man",genre:"Action",year:2008,img:"https://image.tmdb.org/t/p/w500/78lPtwv72eTNqFW9COBYI0dWDJa.jpg"},
-  {title:"Doctor Strange",genre:"Fantasy",year:2022,img:"https://image.tmdb.org/t/p/w500/9Gtg2DzBhmYamXBS1hKAhiwbBKS.jpg"},
-  {title:"Black Panther",genre:"Action",year:2018,img:"https://image.tmdb.org/t/p/w500/uxzzxijgPIY7slzFvMotPv8wjKA.jpg"},
-  {title:"Deadpool",genre:"Comedy",year:2016,img:"https://image.tmdb.org/t/p/w500/3E53WEZJqP6aM84D8CckXx4pIHw.jpg"},
-  {title:"Logan",genre:"Drama",year:2017,img:"https://image.tmdb.org/t/p/w500/fnbjcRDYn6YviCcePDnGdyAkYsB.jpg"},
-  {title:"Aquaman",genre:"Adventure",year:2018,img:"https://image.tmdb.org/t/p/w500/5Kg76ldv7VxeX9YlcQXiowHgdX6.jpg"},
-  {title:"Flash",genre:"Action",year:2023,img:"https://image.tmdb.org/t/p/w500/rktDFPbfHfUbArZ6OOOKsXcv0Bm.jpg"},
-  {title:"Shazam",genre:"Comedy",year:2019,img:"https://image.tmdb.org/t/p/w500/xnopI5Xtky18MPhK40cZAGAOVeV.jpg"},
-  {title:"Venom",genre:"Action",year:2018,img:"https://image.tmdb.org/t/p/w500/2uNW4WbgBXL25BAbXGLnLqX71Sw.jpg"},
-  {title:"Eternals",genre:"Sci-Fi",year:2021,img:"https://image.tmdb.org/t/p/w500/b6qUu00iIIkXX13szFy7d0CyNcg.jpg"},
-  {title:"Matrix",genre:"Sci-Fi",year:1999,img:"https://image.tmdb.org/t/p/w500/aOIuZAjPaRIE6CMzbazvcHuHXDc.jpg"},
-  {title:"John Wick",genre:"Action",year:2014,img:"https://image.tmdb.org/t/p/w500/fZPSd91yGE9fCcCe6OoQr6E3Bev.jpg"},
-  {title:"Inception",genre:"Sci-Fi",year:2010,img:"https://image.tmdb.org/t/p/w500/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"},
-  {title:"Interstellar",genre:"Sci-Fi",year:2014,img:"https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg"},
-  {title:"Avatar",genre:"Sci-Fi",year:2009,img:"https://image.tmdb.org/t/p/w500/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg"},
-  {title:"Titanic",genre:"Romance",year:1997,img:"https://image.tmdb.org/t/p/w500/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg"},
-  {title:"Frozen",genre:"Animation",year:2013,img:"https://image.tmdb.org/t/p/w500/kgwjIb2JDHRhNk13lmSxiClFjVk.jpg"},
-  { title: "Avengers Endgame", genre: "Action", year: 2019, img: "https://image.tmdb.org/t/p/w500/qNBAXBIQlnOThrVvA6mA2B5ggV6.jpg" },
+  {title:"Spider Man",genre:"Action",year:2021,img:"https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", trailer:"https://www.youtube.com/embed/YSrDdNdULOY"},
+  {title:"Joker",genre:"Drama",year:2019,img:"https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg", trailer:"https://www.youtube.com/embed/z6KLY8t7u1E"},
+  {title:"Avengers Endgame",genre:"Action",year:2019,img:"https://image.tmdb.org/t/p/w500/qNBAXBIQlnOThrVvA6mA2B5ggV6.jpg", trailer:"https://www.youtube.com/embed/TcMBFSGVi1c"},
   {title:"Gladiator",genre:"Historical",year:2000,img:"https://image.tmdb.org/t/p/w500/ty8TGRuvJLPUmAR1H1nRIsgwvim.jpg", trailer:"https://www.youtube.com/embed/owK1qxDselE"},
-  { title: "Braveheart", genre: "Historical", year: 1995, img: "https://image.tmdb.org/t/p/w500/or1gBugydmjToAEq7OZY0owwFk.jpg", trailer: "https://www.youtube.com/embed/1NJO0jxBtMo" },
-  { title: "Frozen", genre: "Animation", year: 2013, img:"https://image.tmdb.org/t/p/w500/kgwjIb2JDHRhNk13lmSxiClFjVk.jpg", trailer: "https://www.youtube.com/embed/TbQm5doF_Uc" },
-  {title:"Harry Potter",genre:"Fantasy",year:2001,img:"https://m.media-amazon.com/images/I/81iqZ2HHD-L._AC_SL1500_.jpg", trailer:"https://www.youtube.com/embed/VyHV0BRtdxo"},
-  {title:"Lord of the Rings",genre:"Fantasy",year:2001,img:"https://m.media-amazon.com/images/I/91dSMhdIzTL._SL1500_.jpg", trailer:"https://www.youtube.com/embed/V75dMMIW2B4"},
-{title:"Se7en",genre:"Thriller",year:1995,img:"https://m.media-amazon.com/images/I/71c05lTE03L._AC_SL1024_.jpg", trailer:"https://www.youtube.com/embed/znmZoVkCjpI"}, 
+  {title:"Mad Max Fury Road",genre:"Action",year:2015,img:"https://image.tmdb.org/t/p/w500/8tZYtuWezp8JbcsvHYO0O46tFbo.jpg",trailer:"https://www.youtube.com/embed/hEJnMQG9ev8"},
+  {title:"John Wick",genre:"Action",year:2014,img:"https://image.tmdb.org/t/p/w500/fZPSd91yGE9fCcCe6OoQr6E3Bev.jpg",trailer:"https://www.youtube.com/embed/2AUmvWm5ZDQ"},
+  {title:"Extraction",genre:"Action",year:2020,img:"https://image.tmdb.org/t/p/w500/wlfDxbGEsW58vGhFljKkcR5IxDj.jpg",trailer:"https://www.youtube.com/embed/L6P3nI6VnlY"},
+  {title:"Pathaan",genre:"Action",year:2023,img:"https://upload.wikimedia.org/wikipedia/en/c/c3/Pathaan_film_poster.jpg",trailer:"https://www.youtube.com/embed/vqu4z34wENw"},
+  
+  // 🔵 DRAMA
+  {title:"Forrest Gump",genre:"Drama",year:1994,img:"https://image.tmdb.org/t/p/w500/arw2vcBveWOVZr6pxd9XTd1TdQa.jpg",trailer:"https://www.youtube.com/embed/bLvqoHBptjg"},
+  {title:"The Shawshank Redemption",genre:"Drama",year:1994,img:"https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",trailer:"https://www.youtube.com/embed/6hB3S9bIaco"},
+  {title:"Fight Club",genre:"Drama",year:1999,img:"https://image.tmdb.org/t/p/w500/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg",trailer:"https://www.youtube.com/embed/qtRKdVHc-cE"},
+  {title:"A Beautiful Mind",genre:"Drama",year:2001,img:"https://image.tmdb.org/t/p/w500/zwzWCmH72OSC9NA0ipoqw5Zjya8.jpg",trailer:"https://www.youtube.com/embed/WFJgUm7iOKw"},
+  
+  
+  
+  // 🟢 SCI-FI
+  {title:"The Matrix",genre:"Sci-Fi",year:1999,img:"https://image.tmdb.org/t/p/w500/aOIuZAjPaRIE6CMzbazvcHuHXDc.jpg",trailer:"https://www.youtube.com/embed/vKQi3bBA1y8"},
+  {title:"Avatar",genre:"Sci-Fi",year:2009,img:"https://image.tmdb.org/t/p/w500/kyeqWdyUXW608qlYkRqosgbbJyK.jpg",trailer:"https://www.youtube.com/embed/5PSNL1qE6VY"},
+  {title:"Tenet",genre:"Sci-Fi",year:2020,img:"https://image.tmdb.org/t/p/w500/k68nPLbIST6NP96JmTxmZijEvCA.jpg",trailer:"https://www.youtube.com/embed/L3pk_TBkihU"},
+  {title:"Gravity",genre:"Sci-Fi",year:2013,img:"https://image.tmdb.org/t/p/w500/kZ2nZw8D681aphje8NJi8EfbL1U.jpg",trailer:"https://www.youtube.com/embed/OiTiKOy59o4"},
+  
+  // 🟣 ANIMATION
+  {title:"Toy Story",genre:"Animation",year:1995,img:"https://image.tmdb.org/t/p/w500/uXDfjJbdP4ijW5hWSBrPrlKpxab.jpg",trailer:"https://www.youtube.com/embed/KYz2wyBy3kc"},
+  {title:"Finding Nemo",genre:"Animation",year:2003,img:"https://image.tmdb.org/t/p/w500/eHuGQ10FUzK1mdOY69wF5pGgEf5.jpg",trailer:"https://www.youtube.com/embed/wZdpNglLbt8"},
+  {title:"Coco",genre:"Animation",year:2017,img:"https://image.tmdb.org/t/p/w500/gGEsBPAijhVUFoiNpgZXqRVWJt2.jpg",trailer:"https://www.youtube.com/embed/xlnPHQ3TLX8"},
+  
+  // 🟤 HISTORICAL
+  {title:"Lagaan",genre:"Historical",year:2001,img:"https://upload.wikimedia.org/wikipedia/en/b/b6/Lagaan.jpg",trailer:"https://www.youtube.com/embed/oSIGQ0YkFxs"},
+  
+  // 🟠 FANTASY
+  {title:"Aladdin",genre:"Fantasy",year:2019,img:"https://image.tmdb.org/t/p/w500/3iYQTLGoy7QnjcUYRJy4YrAgGvp.jpg",trailer:"https://www.youtube.com/embed/foyufD52aog"},
+  { title: "The Hobbit", genre: "Fantasy", year: 2012, img: "https://image.tmdb.org/t/p/w500/yHA9Fc37VmpUA5UncTxxo3rTGVA.jpg", trailer: "https://www.youtube.com/embed/SDnYMbYB-nU" },
+  {title:"Se7en",genre:"Thriller",year:1995,img:"https://image.tmdb.org/t/p/w500/6yoghtyTpznpBik8EngEmJskVUO.jpg",trailer:"https://www.youtube.com/embed/znmZoVkCjpI"},
+  { title: "Shutter Island", genre: "Thriller", year: 2010, img: "https://image.tmdb.org/t/p/w500/kve20tXwUZpu4GUX8l6X7Z4jmL6.jpg", trailer: "https://www.youtube.com/embed/5iaYLCiq5RM" },
+  {title:"Pirates of the Caribbean",genre:"Fantasy",year:2003,img:"https://image.tmdb.org/t/p/w500/3E8k4Qy9k7b2t7P4k1i9c5W2Z0m.jpg",trailer:"https://www.youtube.com/embed/naQr0uTrH_s"},
+  {title:"Tanhaji",genre:"Historical",year:2020,img:"./tanaji.jpg", trailer:"https://www.youtube.com/embed/9tB4Rzpbf30"},
+  {title:"Hera Pheri",genre:"Comedy",year:2000,img:"https://upload.wikimedia.org/wikipedia/en/9/9a/Hera_Pheri_%282000_film%29_poster.jpg",trailer:"https://www.youtube.com/embed/m1zMmVwWr-M"},
+  {title:"Dhamaal",genre:"Comedy",year:2007,img:"https://upload.wikimedia.org/wikipedia/en/6/6c/Dhamaal_2007.jpg",trailer:"https://www.youtube.com/embed/-8d0u6lX1zE"},
+  { title: "Golmaal", genre: "Comedy", year: 2006, img: "https://upload.wikimedia.org/wikipedia/en/4/4d/Golmaal_Fun_Unlimited.jpg", trailer: "https://www.youtube.com/embed/1uZ6x7W5q0Q" },
+  {title:"Minions",genre:"Animation",year:2015,img:"https://image.tmdb.org/t/p/w500/q0R4crx2SehcEEQEkYObktdeFy.jpg",trailer:"https://www.youtube.com/embed/eisKxhjBnZ0"},
+  {title:"Pavankhind",genre:"Historical",year:2022,img: "https://upload.wikimedia.org/wikipedia/en/2/28/Pavan_Khind.jpg", trailer:"https://www.youtube.com/embed/2q8V-v5VPPE"},
+  {title:"Jodhaa Akbar",genre:"Historical",year:2008,img:"https://upload.wikimedia.org/wikipedia/en/9/9d/Jodhaa_Akbar.jpg",trailer:"https://www.youtube.com/embed/-H2nJ4zJxQ8"},
+  {title:"Kesari",genre:"Historical",year:2019,img:"https://upload.wikimedia.org/wikipedia/en/4/41/Kesari_poster.jpg",trailer:"https://www.youtube.com/embed/Ji2iKz5U7oE"},
+  // ⚫ THRILLER
+  
+  {title:"Gone Girl",genre:"Thriller",year:2014,img:"https://image.tmdb.org/t/p/w500/gdiLTof3rbPDAmPaCf4g6op46bj.jpg",trailer:"https://www.youtube.com/embed/2-_-1nJf8Vg"},
+  { title: "War", genre: "Action", year: 2019, img: "https://upload.wikimedia.org/wikipedia/en/6/6b/War_official_poster.jpg", trailer: "https://www.youtube.com/embed/tQ0mzXRk-oM" },
+  // 🟡 COMEDY
+  {title:"The Mask",genre:"Comedy",year:1994,img:"https://image.tmdb.org/t/p/w500/6Z0vJ6z2c0E2lE5c5Ny8JD7BqXc.jpg",trailer:"https://www.youtube.com/embed/LZl69yk5lEY"},
+  
+  // ➕ EXTRA MIX (to cross 100 easily duplicate pattern)
+  // Add 100+ more movies here...
 ];
+
+let favorites = [];
+let isLoggedIn = false;
+let currentUser = '';
+
+// Elements
 const container = document.getElementById("movieContainer");
-// 🎬 CREATE CARDS
-movies.forEach(movie => {
-  const card = document.createElement("div");
-  card.classList.add("movie-card");
+const favoritesContainer = document.getElementById("favoritesContainer");
+const favoritesCount = document.getElementById("favoritesCount");
+const favoritesSection = document.getElementById("favoritesSection");
+const loginModal = document.getElementById("loginModal");
+const authSection = document.getElementById("authSection");
+const userWelcome = document.getElementById("userWelcome");
+const themeToggle = document.getElementById("themeToggle");
+const body = document.body;
+const searchInput = document.getElementById("searchInput");
 
-  card.innerHTML = `
-  <div class="wishlist" onclick="toggleLike(this)">
-    <i class="fa fa-heart"></i>
-  </div>
+// Login
+function login() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+  if (username && password) {
+    isLoggedIn = true;
+    currentUser = username;
+    favorites = JSON.parse(localStorage.getItem("favorites_" + username) || "[]");
+    loginModal.style.display = 'none';
+    authSection.style.display = 'flex';
+    userWelcome.textContent = 'Welcome, ' + username + '!';
+    renderMovies(container);
+    renderFavorites();
+  }
+}
 
-  <img src="${movie.img}" 
-       onerror="this.onerror=null; this.src='https://via.placeholder.com/300x450?text=No+Image';">
+// Logout
+function logout() {
+  isLoggedIn = false;
+  loginModal.style.display = 'flex';
+  authSection.style.display = 'none';
+  favoritesSection.style.display = 'none';
+  renderMovies(container);
+}
 
-  <h2>${movie.title}</h2>
-  <p class="genre">Genre: ${movie.genre}</p>
-  <p>Year: ${movie.year}</p>
+// Theme toggle
+function toggleTheme() {
+  body.classList.toggle('light-theme');
+  const isLight = body.classList.contains('light-theme');
+  themeToggle.innerHTML = isLight ? '<i class="fas fa-sun"></i>' : '<i class="fas fa-moon"></i>';
+}
 
-<button class="trailer-btn" onclick="openTrailer('${movie.trailer || ""}')">
-  ▶ Watch Trailer
-</button>
+// Render movies
+function renderMovies(container) {
+  container.innerHTML = '';
+  movies.forEach(movie => {
+    const card = document.createElement('div');
+    card.className = 'movie-card';
+    const isFav = favorites.indexOf(movie.title) > -1;
+    
+    card.innerHTML = 
+      '<div class="wishlist' + (isFav ? ' liked' : '') + '" onclick="toggleFavorite(\'' + movie.title + '\', this)">' +
+      '<i class="fa fa-heart"></i></div>' +
+      '<img src="' + movie.img + '" onerror="this.src=\'https://via.placeholder.com/300x450?text=No+Image\'">' +
+      '<h2>' + movie.title + '</h2>' +
+      '<p class="genre">Genre: ' + movie.genre + '</p>' +
+      '<p>Year: ' + movie.year + '</p>' +
+      '<button class="trailer-btn" onclick="openTrailer(\'' + (movie.trailer || '') + '\')">Trailer</button>' +
+      '<div class="rating">' +
+        '<i class="fa fa-star" data-value="1"></i><i class="fa fa-star" data-value="2"></i><i class="fa fa-star" data-value="3"></i><i class="fa fa-star" data-value="4"></i><i class="fa fa-star" data-value="5"></i>' +
+      '</div><p class="avg">Rating: <span>0</span></p>';
+    
+    container.appendChild(card);
+  });
+}
 
-  <div class="rating">
-    <i class="fa fa-star" data-value="1"></i>
-    <i class="fa fa-star" data-value="2"></i>
-    <i class="fa fa-star" data-value="3"></i>
-    <i class="fa fa-star" data-value="4"></i>
-    <i class="fa fa-star" data-value="5"></i>
-  </div>
+// Toggle favorite
+function toggleFavorite(title, el) {
+  const index = favorites.indexOf(title);
+  if (index > -1) {
+    favorites.splice(index, 1);
+    el.classList.remove('liked');
+  } else {
+    favorites.push(title);
+    el.classList.add('liked');
+  }
+  localStorage.setItem("favorites_" + currentUser, JSON.stringify(favorites));
+  renderFavorites();
+}
 
-  <p class="avg">Rating: <span>0</span></p>
-`;
+// Render favorites
+function renderFavorites() {
+  const favMovies = movies.filter(movie => favorites.includes(movie.title));
+  favoritesContainer.innerHTML = '';
+  favMovies.forEach(movie => {
+    const card = document.createElement('div');
+    card.className = 'movie-card';
+    card.innerHTML = 
+      '<div class="wishlist liked" onclick="toggleFavorite(\'' + movie.title + '\', this)">' +
+      '<i class="fa fa-heart"></i></div>' +
+      '<img src="' + movie.img + '" onerror="this.src=\'https://via.placeholder.com/300x450?text=No+Image\'">' +
+      '<h2>' + movie.title + '</h2>' +
+      '<p class="genre">Genre: ' + movie.genre + '</p>' +
+      '<p>Year: ' + movie.year + '</p>' +
+      '<button class="trailer-btn" onclick="openTrailer(\'' + (movie.trailer || '') + '\')">Trailer</button>' +
+      '<div class="rating">' +
+        '<i class="fa fa-star" data-value="1"></i><i class="fa fa-star" data-value="2"></i><i class="fa fa-star" data-value="3"></i><i class="fa fa-star" data-value="4"></i><i class="fa fa-star" data-value="5"></i>' +
+      '</div><p class="avg">Rating: <span>0</span></p>';
+    favoritesContainer.appendChild(card);
+  });
+  favoritesCount.textContent = '(' + favorites.length + ')';
+  favoritesSection.style.display = favorites.length ? 'block' : 'none';
+}
 
-  container.appendChild(card);
-});
-// ⭐ RATING FIX (dynamic)
-document.addEventListener("click", function(e){
-  if(e.target.classList.contains("fa-star")){
-    let star = e.target;
-    let value = star.getAttribute("data-value");
-
-    let parent = star.parentElement;
-    let allStars = parent.querySelectorAll("i");
-
-    allStars.forEach(s => s.classList.remove("active"));
-
-    for(let i=0;i<value;i++){
-      allStars[i].classList.add("active");
+// Event listeners
+document.addEventListener('click', function(e) {
+  if (e.target.classList.contains('fa-star')) {
+    const value = e.target.getAttribute('data-value');
+    const stars = e.target.parentNode.querySelectorAll('i');
+    stars.forEach(s => s.classList.remove('active'));
+    for (let i = 0; i < value; i++) {
+      stars[i].classList.add('active');
     }
-
-    parent.nextElementSibling.querySelector("span").innerText = value;
+    e.target.parentNode.nextElementSibling.firstChild.textContent = value;
   }
 });
-// 🔍 SEARCH
-document.getElementById("searchInput").addEventListener("keyup", function(){
-  let searchValue = this.value.toLowerCase();
-  let cards = document.querySelectorAll(".movie-card");
+
+searchInput.addEventListener('keyup', function() {
+  const term = this.value.toLowerCase();
+  const cards = container.querySelectorAll('.movie-card');
   cards.forEach(card => {
-    let title = card.querySelector("h2").innerText.toLowerCase();
-    card.style.display = title.includes(searchValue) ? "block" : "none";
+    card.style.display = card.querySelector('h2').textContent.toLowerCase().includes(term) ? 'block' : 'none';
   });
 });
-// 🎯 FILTER
-function filterMovies(category){
-  let cards = document.querySelectorAll(".movie-card");
+
+function filterMovies(category) {
+  const cards = container.querySelectorAll('.movie-card');
   cards.forEach(card => {
-    let genre = card.querySelector(".genre").innerText.toLowerCase();
-    if(category === "all"){
-      card.style.display = "block";
-    }
-    else if(genre.includes(category.toLowerCase())){
-      card.style.display = "block";
-    }
-    else{
-      card.style.display = "none";
-    }
+    const genre = card.querySelector('.genre').textContent.toLowerCase();
+    card.style.display = category === 'all' || genre.includes(category.toLowerCase()) ? 'block' : 'none';
   });
+  document.querySelectorAll('.filter-buttons button').forEach(btn => btn.classList.remove('active'));
+  e.target.classList.add('active');
 }
-function openTrailer(url){
-  document.getElementById("trailerModal").style.display = "flex";
-  document.getElementById("trailerFrame").src = url;
-}
-function closeTrailer(){
-  document.getElementById("trailerModal").style.display = "none";
-  document.getElementById("trailerFrame").src = "";
-}
-function toggleLike(el){
-  el.classList.toggle("liked");
-}
-function openTrailer(url){
-  if(!url){
-    alert("Trailer not available");
+
+function openTrailer(url) {
+  if (!url) {
+    alert('Trailer not available');
     return;
   }
-  document.getElementById("trailerModal").style.display = "flex";
-  document.getElementById("trailerFrame").src = url;
+  document.getElementById('trailerModal').style.display = 'flex';
+  document.getElementById('trailerFrame').src = url;
 }
+
+function closeTrailer() {
+  document.getElementById('trailerModal').style.display = 'none';
+  document.getElementById('trailerFrame').src = '';
+}
+
+// Initialize
+renderMovies(container);
+
